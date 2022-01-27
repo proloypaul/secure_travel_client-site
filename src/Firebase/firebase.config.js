681d92644945ba790991bffd935d9c8e1957b1f3
proloypaul <proloyp23@gmail.com>
2022-01-27 13:38:29 +0600

    connect firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBNS_tzT9z3qbfuaNbNVL_DJhESR8sefP8",
+  
+    authDomain: "travelers-8eafd.firebaseapp.com",
+  
+    projectId: "travelers-8eafd",
+  
+    storageBucket: "travelers-8eafd.appspot.com",
+  
+    messagingSenderId: "50779149114",
+  
+    appId: "1:50779149114:web:a7a6aaa92a974ad072f603"
+  
+  };
+export default firebaseConfig;  
